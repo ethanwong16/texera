16485a7a9c03357284b2810469a2b63ee404b61a
Avinash Kumar <avinash0161@yahoo.com>
2021-02-05 18:10:33 -0800

    some chnages

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sink/SimpleSinkOpExec.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/sink/SimpleSinkOpExec.scala
@@ -3,7 +3,9 @@ package edu.uci.ics.texera.workflow.operators.sink
 import edu.uci.ics.amber.engine.common.tuple.ITuple
 import edu.uci.ics.amber.engine.common.virtualidentity.LinkIdentity
 import edu.uci.ics.amber.engine.common.{ITupleSinkOperatorExecutor, InputExhausted}
+import edu.uci.ics.texera.workflow.common.tuple.Tuple
 
+import java.io.{BufferedWriter, File, FileWriter}
 import scala.collection.mutable
 
 class SimpleSinkOpExec extends ITupleSinkOperatorExecutor {
@@ -11,6 +13,17 @@ class SimpleSinkOpExec extends ITupleSinkOperatorExecutor {
   val results: mutable.MutableList[ITuple] = mutable.MutableList()
 
   def getResultTuples(): Array[ITuple] = {
+//    val file = new File("tweetsOutput.csv")
+//    val bw = new BufferedWriter(new FileWriter(file))
+//    bw.write("bounding_box, sentiment")
+//    for (tuple <- results) {
+//      val bounding_box = tuple.asInstanceOf[Tuple].getField[String]("bounding_box")
+//      val sentiment = tuple.asInstanceOf[Tuple].getField[Int]("sentiment")
+//      bw.write(bounding_box + ", " + sentiment)
+//      bw.newLine()
+//    }
+//    bw.flush()
+//    bw.close()
     results.toArray
   }
 

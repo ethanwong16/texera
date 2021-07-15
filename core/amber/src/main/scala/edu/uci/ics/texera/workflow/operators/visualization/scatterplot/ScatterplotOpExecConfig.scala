ff92b86b1e5bef1aea1d46ee8c7ad3b64afb0ead
sadeemsaleh <sadeem.alsudais@gmail.com>
2021-07-15 01:58:42 -0700

    fix formatting

--- core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/scatterplot/ScatterplotOpExecConfig.scala
+++ core/amber/src/main/scala/edu/uci/ics/texera/workflow/operators/visualization/scatterplot/ScatterplotOpExecConfig.scala
@@ -4,15 +4,19 @@ import edu.uci.ics.amber.engine.architecture.breakpoint.globalbreakpoint.GlobalB
 import edu.uci.ics.amber.engine.architecture.deploysemantics.deploymentfilter.{ForceLocal}
 import edu.uci.ics.amber.engine.architecture.deploysemantics.deploystrategy.{RandomDeployment}
 import edu.uci.ics.amber.engine.architecture.deploysemantics.layer.WorkerLayer
-import edu.uci.ics.amber.engine.common.virtualidentity.{ActorVirtualIdentity, LayerIdentity, OperatorIdentity}
+import edu.uci.ics.amber.engine.common.virtualidentity.{
+  ActorVirtualIdentity,
+  LayerIdentity,
+  OperatorIdentity
+}
 import edu.uci.ics.amber.engine.operators.OpExecConfig
 import edu.uci.ics.texera.workflow.common.tuple.schema.OperatorSchemaInfo
 
 class ScatterplotOpExecConfig(
-                             tag: OperatorIdentity,
-                             opDesc: ScatterplotOpDesc,
-                             schemaInfo: OperatorSchemaInfo
-                           ) extends OpExecConfig(tag) {
+    tag: OperatorIdentity,
+    opDesc: ScatterplotOpDesc,
+    schemaInfo: OperatorSchemaInfo
+) extends OpExecConfig(tag) {
 
   override lazy val topology = new Topology(
     Array(
@@ -28,8 +32,8 @@ class ScatterplotOpExecConfig(
   )
 
   override def assignBreakpoint(
-                                 breakpoint: GlobalBreakpoint[_]
-                               ): Array[ActorVirtualIdentity] = {
+      breakpoint: GlobalBreakpoint[_]
+  ): Array[ActorVirtualIdentity] = {
     topology.layers(0).identifiers
   }
 

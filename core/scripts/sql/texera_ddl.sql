7b790cd0d5b30797ecda8b4bf1041e5dfd688d37
Ethan Wong <wongea2@uci.edu>
2022-01-20 16:20:45 -0800

    updated schema spacing for project tables

--- core/scripts/sql/texera_ddl.sql
+++ core/scripts/sql/texera_ddl.sql
@@ -119,30 +119,30 @@ CREATE TABLE IF NOT EXISTS workflow_version
 
 CREATE TABLE IF NOT EXISTS project
 (
-    `pid`			INT UNSIGNED AUTO_INCREMENT 	NOT NULL,
-    `name` 		    VARCHAR(128) 					NOT NULL,
-    `owner_id`  	INT UNSIGNED 					NOT NULL,
-    `creation_time`	TIMESTAMP						NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    `pid`             INT UNSIGNED AUTO_INCREMENT      NOT NULL,
+    `name`            VARCHAR(128)                     NOT NULL,
+    `owner_id`        INT UNSIGNED                     NOT NULL,
+    `creation_time`   TIMESTAMP                        NOT NULL DEFAULT CURRENT_TIMESTAMP,
     UNIQUE(`owner_id`, `name`),
     PRIMARY KEY (`pid`),
-    FOREIGN KEY (`owner_id`) REFERENCES user (`uid`) ON DELETE CASCADE
+    FOREIGN KEY (`owner_id`) REFERENCES user (`uid`)   ON DELETE CASCADE
 ) ENGINE = INNODB,
   AUTO_INCREMENT = 1;
 
 CREATE TABLE IF NOT EXISTS workflow_of_project
 (
-     `wid`	INT UNSIGNED	NOT NULL,
-     `pid`	INT UNSIGNED	NOT NULL,
+     `wid`            INT UNSIGNED                     NOT NULL,
+     `pid`            INT UNSIGNED                     NOT NULL,
      PRIMARY KEY (`wid`, `pid`),
      FOREIGN KEY (`wid`) REFERENCES `workflow` (`wid`) ON DELETE CASCADE,
-     FOREIGN KEY (`pid`) REFERENCES `project` (`pid`) ON DELETE CASCADE
+     FOREIGN KEY (`pid`) REFERENCES `project` (`pid`)  ON DELETE CASCADE
 ) ENGINE = INNODB;
 
 CREATE TABLE IF NOT EXISTS file_of_project
 (
-     `fid`	INT UNSIGNED	NOT NULL,
-     `pid`	INT UNSIGNED	NOT NULL,
+     `fid`            INT UNSIGNED                     NOT NULL,
+     `pid`            INT UNSIGNED                     NOT NULL,
      PRIMARY KEY (`fid`, `pid`),
-     FOREIGN KEY (`fid`) REFERENCES `file` (`fid`) ON DELETE CASCADE,
-     FOREIGN KEY (`pid`) REFERENCES `project` (`pid`) ON DELETE CASCADE
+     FOREIGN KEY (`fid`) REFERENCES `file` (`fid`)     ON DELETE CASCADE,
+     FOREIGN KEY (`pid`) REFERENCES `project` (`pid`)  ON DELETE CASCADE
 ) ENGINE = INNODB;
